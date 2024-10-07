7e29ec426121105d00eb22ad47dd8ccafec7f11f
accoelhos <anacarolina.sacoelho@gmail.com>
2024-10-07 12:58:09 -0300

    update per pages

--- website/users/users_rotas.py
+++ website/users/users_rotas.py
@@ -8,7 +8,7 @@ from website.adm.models import SPI
 
 @app.route('/')
 def home():
-    per_page = 5
+    per_page = 10
     page = request.args.get('page', type=int, default=1)
     specialissues = SPI.query.order_by(SPI.prazo.asc()).paginate(page=page, per_page=per_page)
     return render_template('users/users_home.html', title='Home para Usuários', specialissues=specialissues)
@@ -34,19 +34,19 @@ def users_search():
         # Atualiza a consulta para incluir tanto título quanto descrição
         specialissues = SPI.query.filter(
             SPI.titulo.like(search) | SPI.detalhes.like(search)
-        ).paginate(page=1, per_page=5)
+        ).paginate(page=1, per_page=20)
         
         return render_template('users/users_busca.html', specialissues=specialissues, search_value=search_value)
     
     else:
         page = request.args.get('page', 1, type=int)
-        per_page = 5
+        per_page = 20
 
         search_value = session.get('search_value', '')  # Obtém da sessão
         if page == 1 and search_value:
             specialissues = SPI.query.filter(
                 SPI.titulo.like(f"%{search_value}%") | SPI.detalhes.like(f"%{search_value}%")
-            ).paginate(page=1, per_page=10)
+            ).paginate(page=1, per_page=20)
         else:
             specialissues = SPI.query.paginate(page=page, per_page=per_page)
 
